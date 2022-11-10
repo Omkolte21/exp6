b51b265329e04e0d94984e3521697c2b3f853c23
Omkolte21 <omkolte594@gmail.com>
2022-11-10 07:52:11 -0800

    exp6

---
+++ src/main/java/Second_Group/om/App.java
@@ -0,0 +1,13 @@
+package Second_Group.om;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Second_Group/om/hii.java
@@ -0,0 +1,7 @@
+package Second_Group.om;
+
+public class hii {
+	public static void main (String [] args ) {
+		System.out.print("welcome om ");
+	}
+}
